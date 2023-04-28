7975974f1386046a95da5f4536fd7e5036abbbc1
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-28 11:00:09 +0900

    htmlMaker 함수 작성 및 테스트용 구문 작성(옵션값이 제대로 적용되는지 테스트)

--- html-maker.js
+++ html-maker.js
@@ -14,7 +14,30 @@ program.command('html')
   .option('-r, --root', 'div#root을 body에 추가함.')
   .option('-p <string>', '해당 html의 본문, 내용', '')
   .action((str, options) =>{
-    
+    const html = htmlMaker(options.title, options.root, options.p);
+    console.log(html);
   })
 
-
+function htmlMaker(title,addRoot, p){
+  let bodyInnerHTML = "";
+  if(addRoot){
+    bodyInnerHTML = `<div id=#root>
+      <p>${p}</p>
+    </div>`
+  } else {
+    bodyInnerHTML = `<p>${p}<p>`
+  }
+  let html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+  </head>
+  <body>
+    ${bodyInnerHTML}
+  </body>
+  </html>`
+  return html;
+}
