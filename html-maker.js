d287e4bb81d0a41ff9645f6fb6dddc7f3b5f82c5
dgchoi3904 <dgchoi3904@gmail.com>
2023-04-28 11:31:17 +0900

    html-maker.js 목표 완료 - 명령어와 옵션을 사용해 파일 생성 확인.

--- html-maker.js
+++ html-maker.js
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import fs from "fs";
 
 const program = new Command();
 
@@ -8,19 +9,25 @@ program
   .version('0.0.1')
 
 program.command('html')
-  .description('새로운 html을 생성해 result에 저장한다.')
+  .description('새로운 html을 생성해 result폴더에 저장한다.')
   .argument('<string>', '생성하는 html 파일명')
   .option('-t, --title <string>', '해당 html의 타이틀', 'Document')
   .option('-r, --root', 'div#root을 body에 추가함.')
   .option('-p <string>', '해당 html의 본문, 내용', '')
-  .action((str, options) =>{
+  .action((str, options) => {
     const html = htmlMaker(options.title, options.root, options.p);
-    console.log(html);
+    try {
+      fs.writeFileSync(`./result/${str}.html`, html, "utf8");
+    } catch (error) {
+      console.error(`${error}가 발생하였습니다.`, error);
+      throw error;
+    }
+
   })
 
-function htmlMaker(title,addRoot, p){
+function htmlMaker(title, addRoot, p) {
   let bodyInnerHTML = "";
-  if(addRoot){
+  if (addRoot) {
     bodyInnerHTML = `<div id=#root>
       <p>${p}</p>
     </div>`
@@ -29,15 +36,15 @@ function htmlMaker(title,addRoot, p){
   }
   let html = `<!DOCTYPE html>
   <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${title}</title>
-  </head>
-  <body>
-    ${bodyInnerHTML}
-  </body>
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>${title}</title>
+    </head>
+    <body>
+      ${bodyInnerHTML}
+    </body>
   </html>`
   return html;
 }
